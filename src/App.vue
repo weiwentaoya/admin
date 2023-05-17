582df0a611ed4b46bb1ec3cb20a3593bda4d1f7b
wentao.wei <wentao.wei@asiainnovations.com>
2023-05-17 16:57:23 +0800

    ✨ feat: 配置文件

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
     <h1>APP</h1>
+    <el-button type="info" :icon="Message" circle />
+    <el-button type="warning" :icon="Star" circle />
   </div>
+  <Test />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Test from "./components/Test.vue";
+import { Message, Star } from "@element-plus/icons-vue";
+</script>
 
 <style scoped></style>
